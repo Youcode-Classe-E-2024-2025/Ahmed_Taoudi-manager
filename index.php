3ead9718934a29ca06efb4d57897ebe6c8edc21d
Tawdi <ahmedtawdi00@gmail.com>
2024-12-17 16:43:51 +0100

    ajoute des routes v.1

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
-<?php 
+<?php
+require('assets/helper/fonctions.php');
+require('assets/database/Database.php');
 
+$uri= parse_url($_SERVER['REQUEST_URI'])['path'];
 
-require('views/index.view.php')
+// dd($uri);
 
-?>
+$routes = [
+    '/'=>'controllers/index.php',
+    '/connecter' => 'controllers/log_sign.php',
+    '/admin' => 'controllers/admin.php',
+];
+
+if(array_key_exists($uri,$routes)){
+    require $routes[$uri];
+}else{
+    require 'views/404.php';
+};
