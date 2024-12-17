e4464d67454ad4362d84c54abbe12bd3a17966af
Tawdi <ahmedtawdi00@gmail.com>
2024-12-17 22:17:29 +0100

    ajoute de sql de creation des tables

--- assets/database/create_database.sql
+++ assets/database/create_database.sql
@@ -5,7 +5,7 @@ create database if not exists rento_car ;
 
 use rento_car ;
 
-/*  */
+/*  table utilisateur  */
 
 create table if not exists utilisateur (
     id int AUTO_INCREMENT PRIMARY KEY ,
@@ -14,10 +14,41 @@ create table if not exists utilisateur (
     date_inscription timestamp DEFAULT CURRENT_TIMESTAMP 
 );
 
-/*  */
+/*  table voiture  */
+create table if not exists voiture (
+    id int AUTO_INCREMENT PRIMARY KEY ,
+    marque VARCHAR(50) not null ,
+    modele VARCHAR(50) not null ,
+    prix_par_jour VARCHAR(50) not null ,
+    disponible boolean default 1 ,
+    image_url VARCHAR(255)
+);
+/*  table reservations  */
+create table if not exists reservations (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    utilisateur_id INT,
+    voiture_id INT,
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+    statut ENUM('pending', 'confirmed', 'cancelled') NOT NULL DEFAULT 'pending',
+    foreign key (utilisateur_id) references utilisateur(id) ,
+    foreign key (voiture_id) references voiture(id) 
+);
 
-/*  */
+/*  table roles  */
 
-/*  */
+create table if not exists roles (
+    id_role INT PRIMARY KEY AUTO_INCREMENT,
+    utilisateur_id  INT,
+    foreign key (utilisateur_id) references utilisateur(id)
+);
 
-/*  */
+/*  table paiements */
+
+create table if not exists paiements (
+    id_paiement INT PRIMARY KEY AUTO_INCREMENT,
+    reservation_id INT,
+    montant DECIMAL(10, 2) NOT NULL,
+    date_paiement DATE NOT NULL,
+   foreign key  (reservation_id)  references reservations(id_reservation)
+);
