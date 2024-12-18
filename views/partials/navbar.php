e4c518d2f39b2048ca4dd0695c983aef2cd34377
Tawdi <ahmedtawdi00@gmail.com>
2024-12-19 00:47:26 +0100

    separer navbar

---
+++ views/partials/navbar.php
@@ -0,0 +1,12 @@
+
+<nav class="bg-blue-600 p-4">
+        <div class="max-w-7xl mx-auto flex justify-between items-center text-white">
+            <div class="text-2xl font-bold">Rento</div>
+            <div class="space-x-6">
+                <a href="/" class="hover:text-gray-300">Home</a>
+                <a href="/cars" class="hover:text-gray-300">Cars</a>
+                <a href="/#about" class="hover:text-gray-300">About</a>
+                <a href="/#contact" class="hover:text-gray-300">Contact</a>
+            </div>
+        </div>
+    </nav>
