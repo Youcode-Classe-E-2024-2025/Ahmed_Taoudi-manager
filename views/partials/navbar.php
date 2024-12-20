14ddbb101574ba90863bccb20ef6fb82478e26f7
Tawdi <ahmedtawdi00@gmail.com>
2024-12-20 03:02:45 +0100

    ajoute des buttons pour login logout signup

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -8,5 +8,14 @@
                 <a href="/#about" class="hover:text-gray-300">About</a>
                 <a href="/#contact" class="hover:text-gray-300">Contact</a>
             </div>
+           <div>
+            <?php if(!isset($_SESSION['username'])) {?>
+           <a class=" px-4 py-2 rounded bg-white text-blue-900" href="/connecter?mtd=login">Log In</a>
+           <a class=" px-4 py-2 rounded bg-white text-blue-900" href="/connecter?mtd=signup">Sign Up</a>
+           <?php }else{ ?>
+            <span class="text-white font-semibold mr-4"><?= $_SESSION['username']  ?> </span>
+            <a class=" px-4 py-2 rounded bg-white text-blue-900" href="/connecter?mtd=logout">Log Out</a>
+            <?php  } ?>
+           </div>
         </div>
     </nav>
