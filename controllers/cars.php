d9e793ad232239ff25435eff3924fd67d0d23cc7
Tawdi <ahmedtawdi00@gmail.com>
2024-12-20 02:57:03 +0100

    fetch cars

--- controllers/cars.php
+++ controllers/cars.php
@@ -1,4 +1,5 @@
 <?php 
 // session_start();
-dd($_SESSION);
+// dd($_SESSION);
+$cars =  $db->query("select * from voiture where disponible = 1 ")->fetchAll(PDO::FETCH_ASSOC);
 require("views/cars.view.php");
