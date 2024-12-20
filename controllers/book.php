20cb0ef2b6c73c895ed9a7c4ad2b29bcf51af2ea
Tawdi <ahmedtawdi00@gmail.com>
2024-12-20 03:01:35 +0100

    ajoute logique de access au  reservation page

--- controllers/book.php
+++ controllers/book.php
@@ -1,5 +1,24 @@
 <?php 
 // session_start();
-dd($_SESSION);
+// dd($_SESSION);
+if(isset($_SESSION['userid'])&&isset($_SESSION['username'])){
+    // userstatus       enum('pending', 'active','archived' )
+   $status = $db->query("select userstatus from user_status where utilisateur_id = :id ",['id'=>$_SESSION['userid']])->fetchColumn();
+//    dd($status);
+if(isset($status)&& $status=='active'){
 
-require("views/book.view.php");
+    require("views/book.view.php");
+}else if(isset($status)&& $status=='pending'){
+    $message = 'your account is pending for this moment ';
+    require("views/message.php");
+}else  if(isset($status)&& $status=='archived'){
+    $message = 'your account is archived for this moment ';
+    require("views/message.php");
+}
+
+}else{ 
+    $message = 'you should log in first';
+    require("views/message.php");
+
+}
+
