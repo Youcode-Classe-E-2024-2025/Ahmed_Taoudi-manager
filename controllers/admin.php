394f0a108b6f7a9e644ed42af20acdab396bb312
Tawdi <ahmedtawdi00@gmail.com>
2024-12-20 03:06:30 +0100

    ajoute logique de route pour side bar de dashboard de admin

--- controllers/admin.php
+++ controllers/admin.php
@@ -1,9 +1,36 @@
-<?php 
+<?php
 // session_start();
-echo "admin controller<br>";
+// echo "admin controller<br>";
 // dd($_SESSION);
-if($_SESSION['userrole']=='admin'){
+if (isset($_SESSION['userrole']) && $_SESSION['userrole'] == 'admin') {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $list;
+
+        if (isset($_GET['show']) && $_GET['show'] == 'cars') {
+            $partial = 'cars';
+            $list = $db->query("select * from voiture")->fetchAll(PDO::FETCH_ASSOC);
+            // dd($list);
+        } else if (isset($_GET['show']) && $_GET['show'] == 'reservations') {
+            $partial = 'reservations';
+            $list = $db->query("select * from reservations")->fetchAll(PDO::FETCH_ASSOC);
+            dd($list);
+        } else if (isset($_GET['show']) && $_GET['show'] == 'users') {
+            $partial = 'users';
+
+            $list = $db->query("select u.* , s.userstatus from utilisateur u left join user_status s on u.id = s.utilisateur_id ")->fetchAll(PDO::FETCH_ASSOC);
+            // dd($list);
+        } else {
+            $partial = 'dashboard';
+           $nbrUsers = $db->query("select COUNT(*) from utilisateur ")->fetchColumn();
+           $nbrOrders = $db->query("select sum(p.montant) from reservations r inner join paiements p on r.id = p.reservation_id ")->fetchColumn();
+           $nbrCars = $db->query("select COUNT(*) from voiture where disponible = 1 ")->fetchColumn();
+            $list = $db->query("select u.* , s.userstatus from utilisateur u left join user_status s on u.id = s.utilisateur_id where s.userstatus = 'pending' ")->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+    //    $user=['name'=>"ahmed",'email'=>"ahmed@aazaz.sasa",'userstatus'=>"active",'date_inscription'=>"2024-12-29"];
     require("views/admin/index.view.php");
-}else{
+} else {
     require('views/404.php');
 }
