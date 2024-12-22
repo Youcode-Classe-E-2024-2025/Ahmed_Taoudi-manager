6fa7a0df44d7b04af60932aa9a5f408c6142ebcd
Tawdi <ahmedtawdi00@gmail.com>
2024-12-22 01:04:59 +0100

    modifier la fonction de affichage de message pour book page

--- controllers/book.php
+++ controllers/book.php
@@ -1,24 +1,19 @@
-<?php 
+<?php
 // session_start();
 // dd($_SESSION);
-if(isset($_SESSION['userid'])&&isset($_SESSION['username'])){
+if (isset($_SESSION['userid']) && isset($_SESSION['username'])) {
     // userstatus       enum('pending', 'active','archived' )
-   $status = $db->query("select userstatus from user_status where utilisateur_id = :id ",['id'=>$_SESSION['userid']])->fetchColumn();
-//    dd($status);
-if(isset($status)&& $status=='active'){
+    $status = $db->query("select userstatus from user_status where utilisateur_id = :id ", ['id' => $_SESSION['userid']])->fetchColumn();
+    //    dd($status);
+    if (isset($status) && $status == 'active') {
+        
+        require("views/book.view.php");
 
-    require("views/book.view.php");
-}else if(isset($status)&& $status=='pending'){
-    $message = 'your account is pending for this moment ';
-    require("views/message.php");
-}else  if(isset($status)&& $status=='archived'){
-    $message = 'your account is archived for this moment ';
-    require("views/message.php");
-}
-
-}else{ 
+    } else if (isset($status) ) {
+        $message = 'your account is '. $status . ' for this moment ';
+        require("views/message.php");
+    }
+} else {
     $message = 'you should log in first';
     require("views/message.php");
-
 }
-
