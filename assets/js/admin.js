5d938551f19528a93359d26e1268cbf1daec05cc
Tawdi <ahmedtawdi00@gmail.com>
2024-12-22 01:03:10 +0100

    ajoute de fonction pour open/close de la form

---
+++ assets/js/admin.js
@@ -0,0 +1,46 @@
+window.openModel = function(event,action){
+    console.log("open object");
+    console.log(event.target.dataset.id);
+ const   id =event.target.dataset.id;
+ document.getElementById('message-model')?.classList.remove('hidden');
+//  console.log(document.getElementById('selected_id')?.value);
+ document.getElementById('selected_id').value = id ;
+ document.getElementById('_action').value = action ;
+ document.getElementById('submit_message').innerHTML="are you sure you want to " + action +" this account ?" ;
+ console.log(document.getElementById('submit_message'));
+//  console.log(document.getElementById('selected_id')?.value); 
+
+}
+window.openModelEditCar = function(event){
+    let id = event.target.dataset.carid;
+
+    fetch(`/req.php?id=${id}`).then(response => response.json())
+    .then(data => {
+
+        console.log(data);
+        const car =data;
+        const form = document.getElementById('form-edit-car');
+        // console.log(form['marque']);
+        form['id'].value = car['id'];
+        form['marque'].value = car['marque'];
+        form['modele'].value = car['modele'];
+        form['matricule'].value = car['matricule'];
+        form['prix_par_jour'].value = car['prix_par_jour'];
+        form['image_url'].value = car['image_url'];
+        form['disponible'].value = car['disponible'];
+        console.log(form);
+        document.getElementById('edit-car-model')?.classList.remove('hidden');
+    });
+
+}
+window.closeModel = function(event){
+// console.log("message-model");
+// console.log(event.target.closest('section'));
+// document.getElementById('message-model').classList.add('hidden');
+event.target.closest('section').classList.add('hidden');
+
+
+}
+
+
+// console.log("object");
