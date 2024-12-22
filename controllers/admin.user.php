164f8fa2973b524c17baeaa4c2d083d041b61671
Tawdi <ahmedtawdi00@gmail.com>
2024-12-22 01:11:08 +0100

    ajoute de fonction de modifier statut d'un utilisateur

---
+++ controllers/admin.user.php
@@ -0,0 +1,21 @@
+<?php
+// session_start();
+// echo "admin controller<br>";
+// dd($_SESSION);
+if (isset($_SESSION['userrole']) && $_SESSION['userrole'] == 'admin') {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // dd($_POST);
+        if (isset($_POST['change_user_status'])) {
+            // dd($_POST);
+            $id =  htmlspecialchars($_POST['selected_id']);
+            $action =  htmlspecialchars($_POST['_action']);
+            $db->query("update user_status set userstatus = :action where utilisateur_id = :id ", ['action' => $action, 'id' => $id]);
+           
+        }
+    }
+
+    header('location: /admin?show=users');
+} else {
+    require('views/404.php');
+}
